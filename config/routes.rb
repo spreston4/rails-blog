e70259c43024990284e2db244112d6b5f3cbc533
Sam Preston <sam.preston@headway.io>
2023-07-21 11:32:11 -0700

    added devise & user model/migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  devise_for :users
+
   resources :blog_posts
   # get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
   # get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
@@ -9,8 +11,6 @@ Rails.application.routes.draw do
   # delete "/blog_posts/:id", to: "blog_posts#destroy"
   # post "/blog_posts", to: "blog_posts#create", as: :blog_posts
  
-
-  # Defines the root path route ("/")
   root "blog_posts#index"
 
 end
