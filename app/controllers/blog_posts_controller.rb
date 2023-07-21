e13528918b800b5e6d90c613a085169552814553
Sam Preston <sam.preston@headway.io>
2023-07-21 12:23:22 -0700

    disabled registration, hid sign in link, edit/new post behind login check

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,6 +1,7 @@
 class BlogPostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_blog_post, except: [:index, :new, :create]
-
+  
   def index
     @blog_posts = BlogPost.all
   end
