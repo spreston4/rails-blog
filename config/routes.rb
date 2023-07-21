543243d53a2cb2adb90e404f0efc421b6323028c
Sam Preston <sam.preston@headway.io>
2023-07-21 11:16:49 -0700

    refactor blog_post routes & controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
-  get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
-  get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
-  patch "/blog_posts/:id", to: "blog_posts#update"
-  delete "/blog_posts/:id", to: "blog_posts#destroy"
-  post "/blog_posts", to: "blog_posts#create", as: :blog_posts
+  resources :blog_posts
+  # get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
+  # get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  # get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
+  # patch "/blog_posts/:id", to: "blog_posts#update"
+  # delete "/blog_posts/:id", to: "blog_posts#destroy"
+  # post "/blog_posts", to: "blog_posts#create", as: :blog_posts
  
 
   # Defines the root path route ("/")
