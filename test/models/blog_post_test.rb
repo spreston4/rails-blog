6fabf305278458e8764f7e3445cfd73b1b49f18d
Sam Preston <sam.preston@headway.io>
2023-07-27 15:12:20 -0700

    added tests for blog_post methods

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,7 +1,39 @@
 require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "draft? returns true for a draft blog post" do
+    assert blog_posts(:draft).draft?
+  end
+
+  test "draft? returns false for a scheduled blog post" do
+    refute blog_posts(:scheduled).draft?
+  end
+
+  test "draft? returns false for a published blog post" do
+    refute blog_posts(:published).draft?
+  end
+
+  test "scheduled? returns false for a draft blog post" do
+    refute blog_posts(:draft).scheduled?
+  end
+
+  test "scheduled? returns true for a scheduled blog post" do
+    assert blog_posts(:scheduled).scheduled?
+  end
+
+  test "scheduled? returns false for a published blog post" do
+    refute blog_posts(:published).scheduled?
+  end
+
+  test "published? returns false for a draft blog post" do
+    refute blog_posts(:draft).published?
+  end
+
+  test "published? returns false for a scheduled blog post" do
+    refute blog_posts(:scheduled).published?
+  end
+
+  test "published? returns true for a published blog post" do
+    assert blog_posts(:published).published?
+  end
 end
